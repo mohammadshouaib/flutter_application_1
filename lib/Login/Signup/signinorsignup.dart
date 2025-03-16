d5e44ed431c03cb84eac95d9a445f0293f0d9362
MeepMeeep1 <shouaibmohammad@gmail.com>
2025-03-16 11:29:53 +0200

    first commit

---
+++ lib/Login/Signup/signinorsignup.dart
@@ -0,0 +1,64 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_application_1/Login/Signup/signin.dart';
+import 'package:flutter_application_1/Login/Signup/signup.dart';
+class SigninOrSignupScreen extends StatelessWidget {
+  const SigninOrSignupScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.white,
+      body: SafeArea(
+        child: Padding(
+          padding: const EdgeInsets.symmetric(horizontal: 16.0),
+          child: Column(
+            children: [
+              const Spacer(flex: 2),
+              Image.network(
+                MediaQuery.of(context).platformBrightness == Brightness.light
+                    ? "https://i.postimg.cc/nz0YBQcH/Logo-light.png"
+                    : "https://i.postimg.cc/MHH0DKv1/Logo-dark.png",
+                height: 146,
+              ),
+              const Spacer(),
+              ElevatedButton(
+                onPressed: () {
+                  Navigator.push(
+                    context,
+                    MaterialPageRoute(builder: (context) => SignInScreen()),
+                  );
+
+                },
+                style: ElevatedButton.styleFrom(
+                  elevation: 0,
+                  backgroundColor: const Color(0xFF00BF6D),
+                  foregroundColor: Colors.white,
+                  minimumSize: const Size(double.infinity, 48),
+                  shape: const StadiumBorder(),
+                ),
+                child: const Text("Sign In"),
+              ),
+              const SizedBox(height: 16.0),
+              ElevatedButton(
+                onPressed: () {
+                  Navigator.push(
+                    context,
+                    MaterialPageRoute(builder: (context) => SignUpScreen()),
+                  );
+                },
+                style: ElevatedButton.styleFrom(
+                    elevation: 0,
+                    foregroundColor: Colors.white,
+                    minimumSize: const Size(double.infinity, 48),
+                    shape: const StadiumBorder(),
+                    backgroundColor: const Color(0xFFFE9901)),
+                child: const Text("Sign Up"),
+              ),
+              const Spacer(flex: 2),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
