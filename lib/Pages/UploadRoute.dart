f13c83d54862cd25dcb44b95609935d0c21ae764
Mahdi Soueid <swdmahdi@gmail.com>
2025-04-28 16:35:56 +0300

    Started progress in the groups page

--- lib/Pages/UploadRoute.dart
+++ lib/Pages/UploadRoute.dart
@@ -4,7 +4,6 @@ import 'package:firebase_storage/firebase_storage.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'dart:io';
-import 'package:image_cropper/image_cropper.dart';
 import 'package:image_picker/image_picker.dart'; // Only if you need image picking
 
 class UploadRoutePage extends StatefulWidget {
